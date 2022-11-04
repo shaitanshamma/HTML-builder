d968c31393e024b0878e0210afaa84b20b6d17f7
shamma <shammacode@gmail.com>
2022-11-04 09:15:17 +0300

    feat: add third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs/promises');
+const path = require('path')
+const stat = require("fs");
+
+async function read(current) {
+    let currentPath = path.join(__dirname, current);
+    try {
+        const files = await fs.readdir(currentPath, {
+            withFileTypes: true
+        });
+        for (const file of files) {
+            if (!file.isDirectory()) {
+                const fileStats = path.join(currentPath, file.name);
+                stat.stat(fileStats, (err, stats) => {
+                    let sizeInKb = Math.ceil(stats.size/1024);
+                    let fileName = path.basename(fileStats,path.extname(fileStats));
+                    let fileExt = path.extname(fileStats).split('.')[1];
+                    let fullFileProp = `${fileName}-${fileExt}-${sizeInKb}kb`;
+                    console.log(fullFileProp)
+                })
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+read('secret-folder');
